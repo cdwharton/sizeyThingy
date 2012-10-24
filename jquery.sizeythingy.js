ddea688f4b52ba06988027641217003132dbe0bb
Chris Wharton <chris@weare2ndfloor.com>
2012-10-24 11:58:34 +0200

    Update jquery.sizeythingy.js

--- jquery.sizeythingy.js
+++ jquery.sizeythingy.js
@@ -1,5 +1,5 @@
 /**
- * jQuery sizeyThingy v1.0
+ * jQuery sizeyThingy v1.1
  * Copyright (C) 2012 Chris Wharton (chris@weare2ndfloor.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -23,33 +23,39 @@
 (function ($) {
     $.sizeyThingy = function (options) {
         var defaults = {
-            sizeyThingyMessage: "W: ",
-            sizeyThingyRelative: false,
-            sizeyForMobileRelative: true,
-            sizeyThingyLight: true
+        	message: "Browser size... ",
+            messageWidth: "W: ",
+            messageHeight: "H: ",
+            relativePosition: false,
+            mobileRelative: true,
+            dark: true
         };
         var options = $.extend(defaults, options);
-        sizeyThingyMessage = options.sizeyThingyMessage;
-        sizeyThingyRelative = options.sizeyThingyRelative;
-        sizeyForMobileRelative = options.sizeyForMobileRelative;
-        sizeyThingyLight = options.sizeyThingyLight;
+        message = options.message;
+        messageWidth = options.messageWidth;
+        messageHeight = options.messageHeight;
+        relativePosition = options.relativePosition;
+        mobileRelative = options.mobileRelative;
+        dark = options.dark;
         //detect known mobile/tablet usage
         if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/Blackberry/i)) || (navigator.userAgent.match(/Windows Phone/i))) {
             var isMobile = true;
         }
-        if (sizeyThingyLight) {
-            sizeyColour = "background:#fff;background:rgba(255,255,255,0.6);color:#000;"
+        if (dark) {
+            sizeyColour = "background:#333;color:#fff;"
         } else {
-            sizeyColour = "background:#000;background:rgba(0,0,0,0.6);color:#fff;"
+            sizeyColour = "background:#fff;color:#333;"
         }
         sizeyThingyWidth = jQuery(window).width();
-        jQuery('body').prepend('<div id="sizeythingy" style="position:fixed;top:0;left:0;width:90%;padding:4px 5%;'+sizeyColour+'font-size:12px;z-index:999;" />');
-        jQuery('#sizeythingy').html(sizeyThingyMessage + sizeyThingyWidth + "px");
+        sizeyThingyHeight = jQuery(window).height();
+        jQuery('body').prepend('<div id="sizeythingy" style="position:fixed;top:0;left:0;width:90%;padding:15px 5%;'+sizeyColour+'font-size:17px;z-index:999;" />');
+        jQuery('#sizeythingy').html(message + messageWidth + sizeyThingyWidth + "px | " + messageHeight + sizeyThingyHeight + "px");
         jQuery(window).resize(function () {
             sizeyThingyWidth = jQuery(window).width();
-            jQuery('#sizeythingy').html(sizeyThingyMessage + sizeyThingyWidth + "px");
+            sizeyThingyHeight = jQuery(window).height();
+            jQuery('#sizeythingy').html(message + messageWidth + sizeyThingyWidth + "px | " + messageHeight + sizeyThingyHeight + "px");
         });
-        if (((isMobile) && (sizeyForMobileRelative)) || (sizeyThingyRelative)) {
+        if (((isMobile) && (mobileRelative)) || (relativePosition)) {
             jQuery('#sizeythingy').css("position", "relative")
         }
        
