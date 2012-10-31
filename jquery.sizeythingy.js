20d535114fc40936f827bce7d560fb4a1e92c23d
Chris Wharton <chris@weare2ndfloor.com>
2012-10-31 11:18:47 +0000

    Added option to position bar at top or bottom

--- jquery.sizeythingy.js
+++ jquery.sizeythingy.js
@@ -1,5 +1,5 @@
 /**
- * jQuery sizeyThingy v1.1
+ * jQuery sizeyThingy v1.2
  * Copyright (C) 2012 Chris Wharton (chris@weare2ndfloor.com)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -28,6 +28,7 @@
             messageHeight: "H: ",
             relativePosition: false,
             mobileRelative: true,
+            positionBottom: false,
             dark: true
         };
         var options = $.extend(defaults, options);
@@ -36,6 +37,7 @@
         messageHeight = options.messageHeight;
         relativePosition = options.relativePosition;
         mobileRelative = options.mobileRelative;
+        positionBottom = options.positionBottom;
         dark = options.dark;
         //detect known mobile/tablet usage
         if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/Blackberry/i)) || (navigator.userAgent.match(/Windows Phone/i))) {
@@ -48,7 +50,12 @@
         }
         sizeyThingyWidth = jQuery(window).width();
         sizeyThingyHeight = jQuery(window).height();
-        jQuery('body').prepend('<div id="sizeythingy" style="position:fixed;top:0;left:0;width:90%;padding:15px 5%;'+sizeyColour+'font-size:17px;z-index:999;" />');
+        if(positionBottom) {
+        	positionThis = "bottom:0;"
+        } else {
+        	positionThis = "top:0;"
+        }
+        jQuery('body').prepend('<div id="sizeythingy" style="position:fixed;'+positionThis+'left:0;width:90%;padding:15px 5%;'+sizeyColour+'font-size:17px;z-index:999;" />');
         jQuery('#sizeythingy').html(message + messageWidth + sizeyThingyWidth + "px | " + messageHeight + sizeyThingyHeight + "px");
         jQuery(window).resize(function () {
             sizeyThingyWidth = jQuery(window).width();
